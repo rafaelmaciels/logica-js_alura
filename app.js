482ac647d380554017d85ab136c0d5265a3a14ab
Rafael Maciel <rafaelmaciels@outlook.com>
2025-01-18 15:38:43 -0300

    Desafio: hora da prática
    Desafios
    Crie um contador que comece em 1 e vá até 10 usando um loop while. Mostre cada número.
    
    Crie um contador que começa em 10 e vá até 0 usando um loop while. Mostre cada número.
    
    Crie um programa de contagem regressiva. Peça um número e conte deste número até 0, usando um loop while no console do navegador.
    
    Crie um programa de contagem progressiva. Peça um número e conte de 0 até esse número, usando um loop while no console do navegador.

--- app.js
+++ app.js
@@ -1,32 +1,37 @@
-alert('Boas vindas ao jogo do número secreto');
-let numeroMaximo = 5000;
-let numeroSecreto = parseInt(Math.random() * numeroMaximo + 1);
-console.log(numeroSecreto);
-let chute;
-let tentativas = 1;
+// Contador que começa em 1 e vai até 10
+let i = 1;
+while (i <= 10) {
+  console.log(i);
+  i++;
+};
 
-// enquanto chute não for igual ao n.s.
-while (chute != numeroSecreto) {
-    chute = prompt(`Escolha um número entre 1 e ${numeroMaximo}`);
-    // se chute for igual ao número secreto
-    if (chute == numeroSecreto) {
-        break;
-    } else {
-        if (chute > numeroSecreto) {
-            alert(`O número secreto é menor que ${chute}`);
-        } else {
-            alert(`O número secreto é maior que ${chute}`);
-        }
-        // tentativas = tentativas + 1;
-        tentativas++;
-    }
+// Contador que começa em 10 e vai até 0:
+let j = 10;
+while (j >= 0) {
+  console.log(j);
+  j--;
+};
+
+//Programa de contagem regressiva:
+let numRegressivo = prompt("Digite um número para a contagem regressiva:");
+numRegressivo = parseInt(numRegressivo);
+
+while (numRegressivo >= 0) {
+  console.log(numRegressivo);
+  numRegressivo--;
+};
+
+// Programa de contagem progressiva:
+let numProgressivo = prompt("Digite um número para a contagem progressiva:");
+numProgressivo = parseInt(numProgressivo);
+let k = 0;
+
+while (k <= numProgressivo) {
+  console.log(k);
+  k++;
 }
 
-let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
-alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}.`);
 
-// if (tentativas > 1) {
-//     alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas.`);
-// } else {
-//     alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativa.`);
-// }
+
+
+
